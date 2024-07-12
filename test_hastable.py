036eba6cd75181974f086a4600bb8e3de2c0556a
bikash119 <bikashjpatra@gmail.com>
2024-07-12 12:48:43 +0530

    TDD using pytest

---
+++ test_hastable.py
@@ -0,0 +1,108 @@
+import pytest
+from hashtable import HashTable,BLANK
+
+# Prepare data that can be used by testcases
+@pytest.fixture
+def hash_table():
+    sample_data = HashTable(size=100)
+    sample_data['hello']='hola'
+    sample_data[98.6] = 37
+    sample_data[False] = True
+    return sample_data
+
+def test_should_always_pass():
+    assert 2+2 == 4, "This is a dummy test"
+
+def test_should_create_hashtable():
+    assert HashTable(size=100) is not None
+
+# How __len__ method of HashTable class
+# is mapped to len() function
+def test_should_report_capacity():
+    assert len(HashTable(size=100)) == 100
+
+def test_should_create_empty_value_slots():
+    assert HashTable(size=3).values == [BLANK,BLANK,BLANK]
+
+def test_should_store_key_and_values():
+    myhashtable = HashTable(size=100)
+    myhashtable['BDFL']="hello"
+    myhashtable[43]=43
+    myhashtable[False]=True
+
+    assert "hello" in myhashtable.values
+    assert 43 in myhashtable.values
+    assert True in myhashtable.values
+
+def test_should_not_increase_when_adding_elements(hash_table):
+    assert len(hash_table) == 100
+
+
+def test_should_not_contain_none_value_when_created():
+    assert None not in HashTable(size=100).values
+
+def test_should_find_value_by_key(hash_table):
+    assert hash_table["hello"] == 'hola'
+    assert hash_table[False] is True
+    assert hash_table[98.6] == 37
+
+def test_should_raise_error_on_missing_key(hash_table):
+    with pytest.raises(KeyError) as exception_info:
+        hash_table['missing_key']
+    assert exception_info.value.args[0] == "missing_key"
+
+def test_should_find_key(hash_table):
+    assert "hello" in hash_table
+
+# how __contains__ is mapped to in keyword?
+def test_should_not_find_key(hash_table):
+    assert "missing_key" not in hash_table
+
+def test_should_get_value(hash_table):
+    assert hash_table.get("hello") == "hola"
+
+def test_should_none_when_missing_key(hash_table):
+    assert hash_table.get("missing_key") is None
+
+def test_should_get_default_value_when_missing_key(hash_table):
+    assert hash_table.get("missing_key","default") == "default"
+
+def test_should_get_value_with_default(hash_table):
+    assert hash_table.get("hello","default") == "hola"
+
+def test_should_delete_key_value_pair(hash_table):
+    assert "hello" in hash_table
+    assert "hola" in hash_table.values
+    assert len(hash_table) == 100
+    
+    del hash_table["hello"]
+    assert "hello" not in hash_table
+    assert "hola" not in hash_table.values
+    assert len(hash_table) == 100
+
+# This decorator allows us to skip a test 
+# during testcase execution
+#@pytest.mark.skip
+def test_should_not_shrink_when_removing_elements(hash_table):
+    assert len(hash_table) == 100
+    del hash_table[False]
+    assert len(hash_table) == 100
+
+def test_should_raise_key_error_when_deleting(hash_table):
+    with pytest.raises(KeyError) as exception_info:
+        del hash_table["missing_key"]
+    assert exception_info.value.args[0] == "missing_key"
+
+def test_should_update_value(hash_table):
+    assert hash_table["hello"] == "hola"
+
+    hash_table["hello"] = "hajime"
+    assert hash_table["hello"] == "hajime"
+    assert hash_table[98.6] == 37
+    assert hash_table[False] == True
+    assert len(hash_table) == 100
+
+def test_should_return_pairs(hash_table):
+    assert ("hello","hola") in hash_table.pairs
+    assert (98.6,37) in hash_table.pairs
+    assert (False,True) in hash_table.pairs
